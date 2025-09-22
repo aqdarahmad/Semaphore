555d199ce48b28a9122d1d36ef1eb5ee286406cc
aqdarahmad <a.abusufyan@student.aaup.edu>
2025-09-22 09:42:06 +0300

    first commit

--- exmple2.sv
+++ exmple2.sv
@@ -11,3 +11,55 @@
 //Prevent the Producer from adding more items than the buffer’s capacity.
 
 //Prevent the Consumer from reading when the buffer is empty.
+
+
+module tst;
+parameter  N = 4;
+int buffer[$];
+
+semaphore mutex = new(1);
+semaphore empty = new(N);
+semaphore full = new(0);
+
+initial begin
+    fork 
+ producer();
+ consumer();
+    join_any
+    #100 $finish;
+    end
+
+  task producer;
+   int item;
+   forever begin 
+    item = $urandom_range(1,100);
+    empty.get(1);
+    mutex.get(1);
+    buffer.push_back(item);
+    $display("[Producer] Produced item %0d at time %0t", item, $time);
+     mutex.put(1);
+     full.put(1);
+     #10;
+     end
+  endtask  
+
+
+  task consumer;
+   int item;
+   forever begin 
+    item = $urandom_range(1,100);
+    full.get(1);
+    mutex.get(1);
+    item = buffer.pop_front();
+     $display("[Consumer] Consumed item %0d at time %0t", item, $time);
+
+     mutex.put(1);
+     empty.put(1);
+     #15;
+     end
+  endtask  
+
+
+
+
+endmodule
